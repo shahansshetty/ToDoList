a47bbf3f3a92f0e458348dddf44e97a7b0d3da98
Shahan S Shetty <shahansshetty@gmail.com>
2025-01-31 22:52:31 +0530

    ui changes

--- working.js
+++ working.js
@@ -2,17 +2,31 @@ let addbutton = document.getElementById('add');
 let inputfeild = document.getElementById('tobox')    
 let outfield = document.getElementById('list-id')    
 
-addbutton.addEventListener('click',function(){
+addbutton.addEventListener('click',async function(){
+   if(inputfeild.value=='')
+   {
+      inputfeild.value="demo";
+   }
+   else{
    var para = document.createElement('li');
+   let del =document.createElement('button');
     para.innerText=inputfeild.value;
     
        outfield.appendChild(para);
+       
+       
        inputfeild.value='';
-       para.addEventListener('click',function(){
-        para.style.textDecoration="line-through";
+       para.addEventListener('click',async function(){
+         if(para.style.textDecoration=="line-through")
+        para.style.textDecoration="none";
+      else
+      para.style.textDecoration="line-through";
        })
-       para.addEventListener('dblclick',function(){
+
+    
+       para.addEventListener('dblclick',async function(){
         outfield.removeChild(para);
        })
+      }
        
 })
